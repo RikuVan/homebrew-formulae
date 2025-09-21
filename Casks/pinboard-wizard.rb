7b3ed29f533e08b33ae70e964af60c196a678edb
rikuvan <richard.vancamp@gmail.com>
2025-09-21 22:32:57 +0300

    Remove interpolation

--- Casks/pinboard-wizard.rb
+++ Casks/pinboard-wizard.rb
@@ -2,10 +2,10 @@ cask 'pinboard-wizard' do
   version '0.1.0'
   sha256 '99ccb1fca86de6baa93e0bb1341c9be88bd6bf188a59ac522a097e918f525b1b'
 
-  url 'https://github.com/RikuVan/pinboard_wizard/releases/download/v#{version}/pinboard-wizard-v#{version}-macos.tar.gz'
+  url 'https://github.com/RikuVan/pinboard_wizard/releases/download/v0.1.0/pinboard-wizard-v0.1.0-macos.tar.gz'
   name 'Pinboard Wizard'
   desc 'A macOS client for Pinboard.in built with AI support and backups'
-  homepage 'https://github.com/rikuvan/pinboard_wizard'
+  homepage 'https://github.com/RikuVan/pinboard_wizard'
 
-  app 'pinboard_wizard.app'
+  app 'Pinboard Wizard.app'
 end
