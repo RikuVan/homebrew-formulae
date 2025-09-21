bf1e070310822a9c2a4d916b9616b8ae5b2e5651
rikuvan <richard.vancamp@gmail.com>
2025-09-21 22:24:38 +0300

    Nest in Casks folder

--- Casks/pinboard-wizard.rb
+++ Casks/pinboard-wizard.rb
@@ -2,7 +2,7 @@ cask 'pinboard-wizard' do
   version '0.1.0'
   sha256 '99ccb1fca86de6baa93e0bb1341c9be88bd6bf188a59ac522a097e918f525b1b'
 
-  url 'https://github.com/rikuvan/pinboard_wizard/releases/download/v#{version}/pinboard-wizard-v#{version}-macos.tar.gz'
+  url 'https://github.com/RikuVan/pinboard_wizard/releases/download/v#{version}/pinboard-wizard-v#{version}-macos.tar.gz'
   name 'Pinboard Wizard'
   desc 'A macOS client for Pinboard.in built with AI support and backups'
   homepage 'https://github.com/rikuvan/pinboard_wizard'
