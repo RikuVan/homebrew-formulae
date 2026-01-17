b364b4a413892b4deb7f6ec94cc455f3b9884f2b
RikuVan <7317771+RikuVan@users.noreply.github.com>
2026-01-17 07:38:32 +0000

    pinboard-wizard: update to v0.7.1

--- Casks/pinboard-wizard.rb
+++ Casks/pinboard-wizard.rb
@@ -1,8 +1,8 @@
 cask 'pinboard-wizard' do
-  version '0.7.0'
-  sha256 "480ac054e8445548020fba490d0773c96922121fbe841cdb16009e4d5623e05e"
+  version '0.7.1'
+  sha256 "0afbeb751ac2c8c535165060772fc1cfd9ed99abe3ddaf7044e59ebcfcb695a2"
 
-  url 'https://github.com/RikuVan/pinboard_wizard/releases/download/v0.7.0/pinboard_wizard.zip'
+  url 'https://github.com/RikuVan/pinboard_wizard/releases/download/v0.7.1/pinboard_wizard.zip'
   name 'Pinboard Wizard'
   desc 'A macOS client for Pinboard.in built with AI support and backups'
   homepage 'https://github.com/RikuVan/pinboard_wizard'
